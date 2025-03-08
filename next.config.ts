16c6dd3e35317e14f34db71a219a719bb7da5101
yongwuk <lyw3059@gmail.com>
2025-03-09 02:43:03 +0900

    init

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from 'next';
+import withPWA from 'next-pwa';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  ...withPWA({
+    dest: 'public',                                   // 서비스 워커 파일의 위치
+    register: true,                                   // 서비스 워커 자동 등록
+    skipWaiting: true,                                // 새로운 서비스 워커가 즉시 활성화되도록 설정
+    disable: process.env.NODE_ENV === 'development',  // 개발 환경에서는 PWA 비활성화
+  }),
+};
+
+export default nextConfig;
---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Daily Retrospect App",
+  description: "This is a PWA built with Next.js",
+  manifest: "/manifest.json",
+  themeColor: "#000000",
+};
+// npm run build && npm run start
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}

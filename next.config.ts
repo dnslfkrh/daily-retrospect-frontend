157b9b7105d5a64532bd07dc71b29c6fefa0044b
yonguk <lyw3059@gmail.com>
2025-06-02 22:28:15 +0900

    feat: ignore eslint
    .ignore는 이제 지원 안한다고함

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,9 @@ const nextConfig: NextConfig = {
     skipWaiting: true,                                // 새로운 서비스 워커가 즉시 활성화되도록 설정
     disable: false //process.env.NODE_ENV === 'development',  // 개발 환경에서는 PWA 비활성화
   }),
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;

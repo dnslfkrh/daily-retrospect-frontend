153419608103dfbf15bec64b385d890083c52dfc
yonguk <lyw3059@gmail.com>
2025-06-03 17:00:14 +0900

    style: prod env..

--- public/env.js
+++ public/env.js
@@ -1,19 +1,19 @@
 // PROD
-// window.env = {
-//   NEXT_PUBLIC_BACKEND_URL: "https://api.myretrodaily.store",
-//   NEXT_PUBLIC_COGNITO_CALLBACK_URL: "https://api.myretrodaily.store/auth/cognito/callback",
-//   NEXT_PUBLIC_COGNITO_LOGOUT_URL: "https://api.myretrodaily.store/auth/cognito/logout",
-//   NEXT_PUBLIC_AWS_REGION: "ap-northeast-2",
-//   NEXT_PUBLIC_COGNITO_DOMAIN: "ap-northeast-29m3ycr9ny",
-//   NEXT_PUBLIC_COGNITO_CLIENT_ID: "53fuv9ej7sd3approg045ltc84"
-// }
-
-// DEV
 window.env = {
-  NEXT_PUBLIC_BACKEND_URL: "http://localhost:8080",
-  NEXT_PUBLIC_COGNITO_CALLBACK_URL: "http://localhost:8080/auth/cognito/callback",
-  NEXT_PUBLIC_COGNITO_LOGOUT_URL: "http://localhost:8080/auth/cognito/logout",
+  NEXT_PUBLIC_BACKEND_URL: "https://api.myretrodaily.store",
+  NEXT_PUBLIC_COGNITO_CALLBACK_URL: "https://api.myretrodaily.store/auth/cognito/callback",
+  NEXT_PUBLIC_COGNITO_LOGOUT_URL: "https://api.myretrodaily.store/auth/cognito/logout",
   NEXT_PUBLIC_AWS_REGION: "ap-northeast-2",
   NEXT_PUBLIC_COGNITO_DOMAIN: "ap-northeast-29m3ycr9ny",
   NEXT_PUBLIC_COGNITO_CLIENT_ID: "53fuv9ej7sd3approg045ltc84"
 }
+
+// DEV
+// window.env = {
+//   NEXT_PUBLIC_BACKEND_URL: "http://localhost:8080",
+//   NEXT_PUBLIC_COGNITO_CALLBACK_URL: "http://localhost:8080/auth/cognito/callback",
+//   NEXT_PUBLIC_COGNITO_LOGOUT_URL: "http://localhost:8080/auth/cognito/logout",
+//   NEXT_PUBLIC_AWS_REGION: "ap-northeast-2",
+//   NEXT_PUBLIC_COGNITO_DOMAIN: "ap-northeast-29m3ycr9ny",
+//   NEXT_PUBLIC_COGNITO_CLIENT_ID: "53fuv9ej7sd3approg045ltc84"
+// }

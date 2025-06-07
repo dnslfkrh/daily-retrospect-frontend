b047c3c8673e4b5d8772f19a763c27e200614cf6
yonguk <lyw3059@gmail.com>
2025-06-07 15:50:50 +0900

    feat: metadata + favicon

--- src/app/(dashboard)/my/(options)/graph/page.tsx
+++ src/app/(dashboard)/my/(options)/graph/page.tsx
@@ -2,6 +2,10 @@
 
 import GoalEvaluationScreen from '@/modules/graph/screens/GoalEvaluationScreen';
 
+export const metadata = {
+  title: "목표 점수 그래프",
+};
+
 const GraphPage = () => {
   return <GoalEvaluationScreen />;
 };
--- src/app/(dashboard)/my/(options)/password/page.tsx
+++ src/app/(dashboard)/my/(options)/password/page.tsx
@@ -1,5 +1,9 @@
 import PasswordChangeScreen from "@/modules/password/screens/PasswordChangeScreen";
 
+export const metadata = {
+  title: "비밀번호 변경",
+};
+
 const PasswordPage = () => {
   return <PasswordChangeScreen />;
 };

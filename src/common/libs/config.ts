0e2d0ee8932f4d8d55b39b3048159e3d7ae3caab
yonguk <lyw3059@gmail.com>
2025-06-13 18:51:43 +0900

    fix: prod env

--- src/common/libs/config.ts
+++ src/common/libs/config.ts
@@ -1,36 +1,15 @@
-// export const config = {
-//   get backendUrl() {
-//     return "https://api.myretrodaily.store"
-//   },
-//   get awsRegion() {
-//     return "ap-northeast-2"
-//   },
-//   get cognitoCallbackUrl() {
-//     return "https://api.myretrodaily.store/auth/cognito/callback"
-//   },
-//   get cognitoLogoutUrl() {
-//     return "https://api.myretrodaily.store/auth/cognito/logout"
-//   },
-//   get cognitoDomain() {
-//     return "ap-northeast-29m3ycr9ny"
-//   },
-//   get cognitoClientId() {
-//     return "53fuv9ej7sd3approg045ltc84"
-//   },
-// };
-
 export const config = {
   get backendUrl() {
-    return "http://localhost:8080"
+    return "https://api.myretrodaily.store"
   },
   get awsRegion() {
     return "ap-northeast-2"
   },
   get cognitoCallbackUrl() {
-    return "http://localhost:8080/auth/cognito/callback"
+    return "https://api.myretrodaily.store/auth/cognito/callback"
   },
   get cognitoLogoutUrl() {
-    return "http://localhost:8080/auth/cognito/logout"
+    return "https://api.myretrodaily.store/auth/cognito/logout"
   },
   get cognitoDomain() {
     return "ap-northeast-29m3ycr9ny"
@@ -39,3 +18,24 @@ export const config = {
     return "53fuv9ej7sd3approg045ltc84"
   },
 };
+
+// export const config = {
+//   get backendUrl() {
+//     return "http://localhost:8080"
+//   },
+//   get awsRegion() {
+//     return "ap-northeast-2"
+//   },
+//   get cognitoCallbackUrl() {
+//     return "http://localhost:8080/auth/cognito/callback"
+//   },
+//   get cognitoLogoutUrl() {
+//     return "http://localhost:8080/auth/cognito/logout"
+//   },
+//   get cognitoDomain() {
+//     return "ap-northeast-29m3ycr9ny"
+//   },
+//   get cognitoClientId() {
+//     return "53fuv9ej7sd3approg045ltc84"
+//   },
+// };
